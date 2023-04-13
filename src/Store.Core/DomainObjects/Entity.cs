bd4940c8b99f81288441d55160144df1eb8fcb6e
realrgt <ergitorubildo@gmail.com>
2023-04-13 15:13:41 +0200

    Sales - Order Command Handler

--- src/Store.Core/DomainObjects/Entity.cs
+++ src/Store.Core/DomainObjects/Entity.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Store.Core.Messages;
+using System;
+using System.Collections.Generic;
 
 namespace Store.Core.DomainObjects
 {
@@ -6,9 +8,27 @@ namespace Store.Core.DomainObjects
     {
         public Guid Id { get; set; }
 
+        private List<Event> _notifications;
+        public IReadOnlyCollection<Event> Notifications => _notifications?.AsReadOnly();
+
         protected Entity()
         {
             Id = Guid.NewGuid();
+        }
+
+        public void AddEvent(Event evnt) {
+            _notifications = _notifications ?? new List<Event>();
+            _notifications.Add(evnt);
+        }
+
+        public void RemoveEvent(Event evnt)
+        {
+            _notifications?.Remove(evnt);
+        }
+
+        public void ClearEvents()
+        {
+            _notifications?.Clear();
         }
     }
 }
--- src/Store.Sales.Application/Commands/OrderCommandHandler.cs
+++ src/Store.Sales.Application/Commands/OrderCommandHandler.cs
@@ -25,11 +25,9 @@ namespace Store.Sales.Application.Commands
 
             _orderRepository.Add(order);
 
-            await _mediator.Publish(
-                new OrderItemAddedEvent(order.CustomerId, order.Id, message.ProductId, message.Name, message.UnitPrice, message.Quantity), 
-                cancellationToken);
+            order.AddEvent(new OrderItemAddedEvent(order.CustomerId, order.Id, message.ProductId, message.Name, message.UnitPrice, message.Quantity));
 
-            return true;
+            return await _orderRepository.UnitOfWork.Commit();
         }
     }
 }
