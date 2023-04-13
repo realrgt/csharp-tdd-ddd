704aaddb0f5b08cd65f6d44aadb99ddb4d603ebf
realrgt <ergitorubildo@gmail.com>
2023-04-13 15:35:12 +0200

    Infra layer setup

---
+++ src/Store.Sales.Data/MediatorExtension.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Store.Core.DomainObjects;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Sales.Data
+{
+    public static class MediatorExtension
+    {
+        public static async Task PublishEvents(this IMediator mediator, SalesContext ctx)
+        {
+            var domainEntities = ctx.ChangeTracker
+                .Entries<Entity>()
+                .Where(x => x.Entity.Notifications != null && x.Entity.Notifications.Any());
+
+            var domainEvents = domainEntities
+                .SelectMany(x => x.Entity.Notifications)
+                .ToList();
+
+            domainEntities.ToList()
+                .ForEach(entity => entity.Entity.ClearEvents());
+
+            var tasks = domainEvents
+                .Select(async (domainEvent) => {
+                    await mediator.Publish(domainEvent);
+                });
+
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ src/Store.Sales.Data/SalesContext.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Store.Core.Data;
+using System.Threading.Tasks;
+
+namespace Store.Sales.Data
+{
+    public class SalesContext : DbContext, IUnitOfWork
+    {
+
+        private readonly IMediator _mediator;
+
+        public SalesContext(DbContextOptions<SalesContext> options, IMediator mediator) : base(options)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<bool> Commit()
+        {
+            var success = await base.SaveChangesAsync() > 0;
+            if (success) await _mediator.PublishEvents(this);
+
+            return success;
+        }
+    }
+}
